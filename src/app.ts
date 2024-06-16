49598b8269606a48b177696baef16b9a4d8dc852
Yarizk <yazidrizkik@gmail.com>
2024-06-16 17:07:59 +0700

    add size limit

--- src/app.ts
+++ src/app.ts
@@ -94,8 +94,8 @@ initPassport(app);
 
 
 
-
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 app.use(cors());
 app.use(errors());
 app.use('/api/auth', authRoutes);
